175ee3429b3566c5aef45fafc95390fbc5f429a6
Puneeth K <puneethk.2899@gmail.com>
2019-10-01 21:07:47 +0530

    Added routes for check in page

--- checkin/urls.py
+++ checkin/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
- 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', views.home),
-    path('checkin/', include('checkin.urls'))
+    path('', views.check_in),
 ]
--- checkin/views.py
+++ checkin/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+# render check in view.
+def check_in(request):
+    return render(request, 'check_in.html', {})
