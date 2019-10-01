e6bdc58cfcfdceb2894513a593dc4f71140eb3d5
Puneeth K <puneethk.2899@gmail.com>
2019-10-01 19:10:47 +0530

    Created route for checkin page

--- portal/views.py
+++ portal/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {})
+
+# to render the check in page assuming that it is created
+def check_in(request):
+    return render(request, 'checkin.html', {})
