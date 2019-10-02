efabbfef78f4dda3635157013a2230c0d75dfa32
Swarup <swaruphegde@gmail.com>
2019-10-02 11:52:08 +0530

    updated events model with necessary changes

--- checkin/models.py
+++ checkin/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Events(models.Model): 
+    #event_id will store spreadsheet link for unique identification of event 
+    event_id = models.CharField(max_length=500, unique=True) 
+    event_name = models.CharField(max_length=100) 
+    total_attendees = models.IntegerField(default=0) 
+    check_in_count = models.IntegerField(default=0) 
+    created_at = models.DateTimeField(blank=True, null=True) 
+    updated_at = models.DateTimeField(blank=True, null=True) 
+    
+    def create(self): 
+        self.created_at = timezone.now() 
+        self.save() 
+    
+    def update(self): 
+        self.updated_at = timezone.now() 
+        self.save() 
+    
+    def __str__(self): 
+        return self.event_name
