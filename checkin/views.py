9ab9f21e0e25c97a66808d2d30ef32cdecfc3c6d
Harsh Borse <47155520+borsezf2@users.noreply.github.com>
2019-10-03 13:32:45 +0530

    push from new repo (#14)
    * push from new repo
    
    * html edited
    
    * requiremnts and json free formate
    
    * test commit
    
    * gsspread corrected
    
    * check

--- checkin/views.py
+++ checkin/views.py
@@ -1,6 +1,41 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 # Create your views here.
 # render check in view.
 def check_in(request):
-    return render(request, 'check_in.html', {})
+    if request == 'POST':
+        checkinGen(request)
+    else:
+        return render(request, 'check_in.html', {})
+
+jsonDic = {
+
+}
+def checkinGen(request):
+    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    print("\n\n\n____________________________________________________________________________________________________")
+    print("checkin gen")
+    print("START\n\n")
+    # creds = ServiceAccountCredentials.from_json_keyfile_name("checkin/checkin-creds.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(jsonDic, scope)
+    client = gspread.authorize(creds)
+    filename = request.POST['filename']
+    sheet = client.open(filename).sheet1 # Open the spreadhseet
+    fields = sheet.get_all_values()  # get all the data into a list formate
+
+    print("You have these Columns\n", fields[0])  # printing the column names
+
+    print("\nand data entered are\n")
+
+    # printing the data entered by people
+    for i in range(1, len(fields)):
+        print("\n")
+        print(fields[i])
+
+    print("\n\n END")
+    print("____________________________________________________________________________________________________\n\n\n")
+    return redirect('/checkin')
+
--- portal/views.py
+++ portal/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {})
+
+
